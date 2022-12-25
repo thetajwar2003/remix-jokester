190e6f03eb52c8bf54bf16c719fd38a1d7c00df0
thetajwar2003 <rahmantajwar131@gmail.com>
2022-12-25 08:17:25 -0500

    added routes and styling

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,24 @@
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Title: <input type="text" name="title" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
